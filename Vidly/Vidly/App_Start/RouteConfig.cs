6eaede0a87adbeb751a4863e94685442a76493dd
Ramkumar G <gk.ramkumar@gmail.com>
2022-02-16 15:20:37 +0530

    * Adding Movies Controller * Updating bootstrap CSS file * Updating Route config

--- Vidly/Vidly/App_Start/RouteConfig.cs
+++ Vidly/Vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "SearchByYear",
+                "movies/search/{year}/{month}",
+                new { Controller = "Movies", Action = "Search" },
+                new { year = @"\d{4}", month = @"\d{2}"}
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
---
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Incredibles" };
+
+            //return View(movie);
+
+            //return Content("Test message to test Content ActionResult type");
+            //return HttpNotFound();
+            //return RedirectToAction("Index", "Home", new { code = 1, sortby = "age" });
+            return new EmptyResult();
+        }
+
+        public ActionResult Edit(int moviesId)
+        {
+            return Content($"Url parameter is {moviesId}");
+        }
+
+        public ActionResult Index(int? pageIndex, string category)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(category))
+                category = "main";
+
+            return Content($"Parameters values are PageIndex : {pageIndex}, Category : {category}.");
+        }
+
+        public ActionResult Search(int year, short month)
+        {
+            return Content($"Search parameters are year {year}, month {month}");
+        }
+          
+    }
+}
